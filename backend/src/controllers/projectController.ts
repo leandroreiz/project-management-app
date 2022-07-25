e1e338db3e814c1363151397af417604fb52f54b
Leandro Reis <leandro.reiz@gmail.com>
2022-07-25 23:22:23 +0100

    Add update and delete functions to API

--- backend/src/controllers/projectController.ts
+++ backend/src/controllers/projectController.ts
@@ -1,20 +1,61 @@
 import { RequestHandler } from 'express';
 import Project from '../models/projectModel';
 
-export const getProjects: RequestHandler = async (req, res, next) => {
-  const projects = await Project.find();
-
-  res.status(200).json({
-    status: 'success',
-    projects,
-  });
-};
-
+// Create documents
 export const createProject: RequestHandler = async (req, res, next) => {
-  const doc = await Project.create(req.body);
+  const data = await Project.create(req.body);
 
   res.status(201).json({
     status: 'success',
-    data: doc,
+    data,
+  });
+};
+
+// Read document
+export const getProject: RequestHandler = async (req, res, next) => {
+  const data = await Project.findById(req.params.id);
+
+  if (!data) throw new Error('No document found with that ID');
+
+  res.status(200).json({
+    status: 'success',
+    data,
+  });
+};
+
+// Read documents
+export const getProjects: RequestHandler = async (req, res, next) => {
+  const data = await Project.find();
+
+  res.status(200).json({
+    status: 'success',
+    data,
+  });
+};
+
+// Update document
+export const updateProject: RequestHandler = async (req, res, next) => {
+  const data = await Project.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!data) throw new Error('No document found with that ID');
+
+  res.status(200).json({
+    status: 'success',
+    data,
+  });
+};
+
+// Delete document
+export const deleteProject: RequestHandler = async (req, res, next) => {
+  const data = await Project.findByIdAndDelete(req.params.id);
+
+  if (!data) throw new Error('No document found with that ID');
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
   });
 };
--- backend/src/routes/projectRoutes.ts
+++ backend/src/routes/projectRoutes.ts
@@ -1,9 +1,25 @@
 import express from 'express';
-import { createProject, getProjects } from '../controllers/projectController';
+import {
+  createProject,
+  deleteProject,
+  getProject,
+  getProjects,
+  updateProject,
+} from '../controllers/projectController';
 
 const router = express.Router();
 
-router.get('/', getProjects);
+// Create document
 router.post('/new', createProject);
+
+// Read document(s)
+router.get('/:id', getProject);
+router.get('/', getProjects);
+
+// Update document
+router.put('/update/:id', updateProject);
+
+// Delete document
+router.delete('/delete/:id', deleteProject);
 
 export default router;
